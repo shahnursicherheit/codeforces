d6f9e6bafb8e1cc1b10ddb108f420d40c0ddde07
shahnursicherheit <mdshahnur05@gmail.com>
2024-11-07 02:30:03 +0600

    update code

--- A. Ostap and Grasshopper.cpp
+++ A. Ostap and Grasshopper.cpp
@@ -2,13 +2,53 @@
 using namespace std ;
 int main ()
 {
-    int  n , m ;
+    int  n , m , t , g ;
     cin >> n >> m ;
-    int a[n] ;
+    string a ;
+    cin >> a ;
     for ( int i = 0 ; i < n ; i++ )
     {
-        cin >> a[i] ;
+        if ( a[i] == 'G' )
+        {
+            g = i ;
+        }
+        else if ( a[i] == 'T' )
+        {
+            t = i ;
+        }
+    }
+    if ( t < g )
+    {
+        int temp = t ;
+        t = g ;
+        g = temp ;
+        a[t] = 'T' ;
     }
 
 
+    for ( int i = g ; i < n ; i = i + m )
+    {
+        if ( ( a[i+m] == '.' || a[i+m] == 'T' ) && ( i + m ) <= t )
+        {
+            g += m ;
+        }
+
+
+    }
+    if ( g == t )
+        {
+            cout << "YES" << endl ;
+        }
+        else
+        {
+            cout << "NO" << endl ;
+        }
+
+
 }
+/*
+100 20
+
+.....G.#.#..................#.........##....#...................#.#..........#...#.#.T..#.......#...
+
+*/
