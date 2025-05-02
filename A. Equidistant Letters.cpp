968bbf860529ae93b1daf86e9737e88a289ebd78
shahnursicherheit <mdshahnur05@gmail.com>
2025-05-02 18:47:13 +0600

    basic sort problem

---
+++ A. Equidistant Letters.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int t ;
+    cin >> t ;
+    while ( t-- )
+    {
+        string s, s2 ;
+        cin >> s;
+        int sze = s.length(), count = 1 ;
+        sort(s.begin(),s.end()) ;
+        s2 = s ;
+        cout << s << endl ;
+
+        /*for ( int i = 0 ; i < sze ; i++ )
+        {
+            if ( s[i] == s[i+1] )
+            {
+                s2[i+2]
+            }
+        }*/
+
+    }
+}
